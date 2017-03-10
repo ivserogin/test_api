8f57c446b01a130d67725b60f932225ccb609378
ivserogin <infosivin@yahoo.com>
2017-03-10 12:49:39 +0200

    all files

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 const noteRoutes = require ('./note_routes');
 module.exports = function (app, db){
   noteRoutes(app, db);
-  
+
+};
+module.exports = function (app, db){
+  app.post('/notes', (req,res) =>{
+
+
+    res.send('Hello');
+  });
 };
--- routes/note_routes.js
+++ routes/note_routes.js
@@ -1,5 +1,18 @@
 module.export = function (app, db){
 app.post('./notes',(req, res)=>{
-  res.send('hello')
+  const note = {tetx: req.body.body, title: req.body.title};
+  db.collection('notes').insert(note, (err, result)=>{
+    if (err) {
+      res.send ({'error':'An error has occurred'});
+    }else {
+      res.send(result.ops[0]);
+    }
+  });
 });
 };
+/*module.export = function (app, db){
+  app.post('./notes', (req, res)=>{
+    console.log(req.body);
+    res.send('Hello');
+  });
+};*/
