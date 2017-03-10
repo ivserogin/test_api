5c4a64b78aa2ba3b34c3337fa40de714ae908a2b
ivserogin <infosivin@yahoo.com>
2017-03-10 12:44:35 +0200

    edit server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require ('express');
 const MongoClient = require ('mongodb').MongoClient;
 const bodyParser = require ('body-parser');
+const db = require ('./config/db');
 const app = express ();
 
 const port = 8000;
-require('./routes')(app,{});
-app.listen (port, ()=>{
-  console.log("This app on " + port);
+app.use(bodyParser.urlencoded({ extended: true }));
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  require('./routes')(app, database);
+  app.listen(port, () => {
+    console.log("This app on " + port);
+  });
 });
